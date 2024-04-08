585dc4d88d5553cced9343583d432d12adc1a5bc
petrovjelena <97536660+petrovjelena@users.noreply.github.com>
2024-04-08 06:19:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,8 @@ setup(
     scripts=['install.sh'],
     install_requires=[
         "setuptools>=44.1",
+        "argparse>=1.4.0",
+        "pillow>=9.0.0",
+        "requests>=2.25.1",
     ],
 )
